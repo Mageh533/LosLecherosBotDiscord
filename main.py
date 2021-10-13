99427822b129ad3d6f1f5cab09381985d679fc3e
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2021-10-13 21:31:23 +0100

    Fixed commands not working

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import discord
 from discord.ext import commands
 
 token = "ODk3OTExNDYxOTY2NDAxNTg2.YWcjNg.oEyCG0BQqIBODVKGKrjPt0OXWp0"
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '-')
 
 @client.event
 async def on_ready():
   print("Bot is now logged in as {0.user}".format(client))
-
-@client.event
-async def on_message(message):
-  if message.author == client:
-    return
-  
-  if message.content.startswith("Quien es paco?"):
-    await message.channel.send("El mejor bot del mundo creado por el mejor programador lechero que existe")
 
 @client.command(pass_context = True)
 async def join(ctx):
