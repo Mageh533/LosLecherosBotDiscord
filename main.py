a0c36ec63be918964249fdbf15dfffc65fd5358e
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2021-10-13 21:28:19 +0100

    Added join and leave commands

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
+from discord.ext import commands
 
 token = "ODk3OTExNDYxOTY2NDAxNTg2.YWcjNg.oEyCG0BQqIBODVKGKrjPt0OXWp0"
-client = discord.Client()
+client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
@@ -12,7 +13,23 @@ async def on_message(message):
   if message.author == client:
     return
   
-  if message.content.startswith("-play"):
-    await message.channel.send("Playing music")
+  if message.content.startswith("Quien es paco?"):
+    await message.channel.send("El mejor bot del mundo creado por el mejor programador lechero que existe")
+
+@client.command(pass_context = True)
+async def join(ctx):
+  if (ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+  else:
+    await ctx.send("Oye tonto que tal si entras en un canal de voz antes de usar ese comando?")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+  if (ctx.voice_client):
+    await ctx.guild.voice_client.disconnect()
+    await ctx.send("Deu")
+  else:
+    await ctx.send("No estoy en ningun canal de voz")
 
 client.run(token)
