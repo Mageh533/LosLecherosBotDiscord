dc4b8aa1e3442552455c8dc872491fad309aaa03
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2021-10-13 23:07:35 +0100

    Added audio when bot join
    ameno is a placeholder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import FFmpegPCMAudio
 
 token = "ODk3OTExNDYxOTY2NDAxNTg2.YWcjNg.oEyCG0BQqIBODVKGKrjPt0OXWp0"
 client = commands.Bot(command_prefix = '-')
@@ -9,15 +10,17 @@ async def on_ready():
   print("Bot is now logged in as {0.user}".format(client))
 
 @client.command(pass_context = True)
-async def join(ctx):
+async def play(ctx):
   if (ctx.author.voice):
     channel = ctx.message.author.voice.channel
-    await channel.connect()
+    voice = await channel.connect()
+    source = FFmpegPCMAudio('audio/Ameno.mp3')
+    player = voice.play(source)
   else:
     await ctx.send("Oye tonto que tal si entras en un canal de voz antes de usar ese comando?")
 
 @client.command(pass_context = True)
-async def leave(ctx):
+async def stop(ctx):
   if (ctx.voice_client):
     await ctx.guild.voice_client.disconnect()
     await ctx.send("Deu")
